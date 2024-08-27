4a0bfcfe9f0a4a170c3bc91b20c2ead136019518
Sridhar <katukuri-naga.sridhar@capgemini.com>
2024-08-27 19:11:00 +0530

    forloop

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
-echo "s"
-echo "h"
-echo "r"
-echo "managali"
-echo "devops"
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "s"
+# echo "h"
+# echo "r"
+# echo "managali"
+# echo "devops"
+
+
+# for i in 1 2 4 5 6 
+# do
+#     echo$i
+# done
+
+for j in {0..10}
+do
+    echo$j
+done
+
+
